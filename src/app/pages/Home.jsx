517998e197b3eb6006b3459e565765c1f8fea0a6
Franco Gonzalez <gonzalezf503@gmail.com>
2022-11-08 11:55:59 -0300

    home.jsx updated & minor fixes

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,25 +1,20 @@
-import { Link } from "react-router-dom"
+
 
 export const Home = () => {
     return (
     <div className="container mt-5 animate__animated animate__fadeIn">
     
         <div className="my-4 bg-white p-4 shadow">
-            <h1>Hola</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt nam, amet velit dicta minus fugit corporis facere consequatur eveniet qui obcaecati facilis cumque quae error dolor ratione. Tempora, a impedit.</p>
+            <h1 className="mb-4">Bienvenido</h1>
+            <p>
+                Este es un sitio de prueba, usado especialmente para el evento a ocurrir en Mendoza. <br />
+                Los estilos no son los finales, y la mayoría de las funciones han sido recortadas.
+                <br />
+                <strong>
+                    Accedé a las canciones desde la barra de navegación.
+                </strong>
+            </p>
         </div>
-        <div className="grid">
-            <Link to='/' className="item">
-                Setlist
-            </Link>
-            <Link to='/alabanzas' className="item">
-                Alabanzas
-            </Link>
-            <Link to='/adoraciones' className="item">
-                Adoraciones
-            </Link>
-        </div>
-        
     </div>
     )
 }
